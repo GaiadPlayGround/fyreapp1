94cfe7b36bd6986f552c9bb863e5d0f745429e94
HawkNode <1hawknode@gmail.com>
2026-01-31 13:00:54 +0000

    mini app modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider } from 'wagmi';
+import { WagmiProvider } from "wagmi";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { WalletProvider } from "@/contexts/WalletContext";
 import { config } from "@/lib/walletConfig";
+
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import SpeciesDetail from "./pages/SpeciesDetail";
@@ -15,29 +19,37 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <WagmiProvider config={config} reconnectOnMount={false}>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <WalletProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/connect" element={<WalletGate />} />
-                <Route path="/explore" element={<Explore />} />
-                <Route path="/explore/:speciesId" element={<SpeciesDetail />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </TooltipProvider>
-        </WalletProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
-);
+const App = () => {
+  useEffect(() => {
+    sdk.actions.ready().catch((error) => {
+      console.error("Failed to call sdk.actions.ready()", error);
+    });
+  }, []);
+
+  return (
+    <WagmiProvider config={config} reconnectOnMount={false}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <WalletProvider>
+            <TooltipProvider>
+              <Toaster />
+              <Sonner />
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/connect" element={<WalletGate />} />
+                  <Route path="/explore" element={<Explore />} />
+                  <Route path="/explore/:speciesId" element={<SpeciesDetail />} />
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </BrowserRouter>
+            </TooltipProvider>
+          </WalletProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
 
 export default App;
